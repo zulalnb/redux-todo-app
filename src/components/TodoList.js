622e26b473682c868589ba4d283ca577ce9e1c49
Zülal Nebin <zllnbn1997@gmail.com>
2022-04-10 23:57:34 +0300

    delete todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,20 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
 
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
+
   return (
     <ul className="todo-list">
-      {/* <li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-      </li> */}
-      {/* <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn React</label>
-          <button className="destroy"></button>
-        </div>
-      </li> */}
-      {/* <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Have a life!</label>
-          <button className="destroy"></button>
-        </div>
-      </li> */}
-
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
@@ -41,7 +25,9 @@ function TodoList() {
               onChange={() => dispatch(toggle({ id: item.id }))}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}></button>
           </div>
         </li>
       ))}
