b005383ae8ed4c79b7f7e04f1a2deb2ef8deef1e
Zülal Nebin <zllnbn1997@gmail.com>
2023-12-16 19:28:36 +0000

    create store and todos slice

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
---
+++ src/redux/todos/todosSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    items: [
+      {
+        id: 1,
+        title: "Learn JavaScript",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Learn React",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Have a life!",
+        completed: false,
+      },
+    ],
+  },
+  reducers: {},
+});
+
+export default todosSlice.reducer;
